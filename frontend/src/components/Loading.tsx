3b336c174b6bed2f46ce419cb5bd430e4f0129a0
Guilherme Gavioli <xiiguilhermeiix@gmail.com>
2022-05-23 00:10:53 -0300

    create loading component

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,17 @@
+import { CubeIcon} from "@heroicons/react/solid"
+
+
+
+  
+export default function Loading() { 
+    return (
+
+    <div className="w-10 h-10 m-auto pt-[15%] ">
+            
+
+            <CubeIcon className=" text-white animate-spin "/>
+           
+    </div>
+   
+    )
+  }
