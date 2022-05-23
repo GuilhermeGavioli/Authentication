a7dd2040f8d205db135f1cdcbfbffb2e01b85b8a
Guilherme Gavioli <xiiguilhermeiix@gmail.com>
2022-05-23 00:10:18 -0300

    Protect page w/ useEffect verifying if there is a auth-cookie & if it is valid + Add loading component on page loading + show a message to the user when request fails & on implements handleRegister on register page.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,16 +3,36 @@ import { LockClosedIcon, MailIcon } from '@heroicons/react/solid'
 //components
 import Input from '../components/Input';
 import Button from '../components/Button';
-import { Link } from 'react-router-dom';
-import { useState, createContext } from 'react';
-export const LoginDataContext = createContext("LoginDataContext");
+import Loading from '../components/Loading';
 
+import { Link } from 'react-router-dom';
+import { useCookies } from 'react-cookie'
+import { useState, useEffect } from 'react';
+// export const LoginDataContext = createContext("LoginDataContext");
+import { Navigate, useNavigate } from 'react-router-dom'
 
 
 export default function Login() {
   const [ loginEmail, setLoginEmail ] = useState("");
   const [ loginPassword, setLoginPassword ] = useState("");
-  const [ errorMessage, setErrorMessage ] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [cookies, setCookie, removeCookie] = useCookies(['Authorization-cookie'])
+  const [pageLoading, setPageLoading] = useState(true);
+  
+  const navigate = useNavigate();
+  const redirect = () => navigate("/dashboard");
+  
+  useEffect(() => {
+    async function check() {
+      if (await cookies['Authorization-cookie'] === undefined) return;
+      const res = await fetch("http://localhost:3001/account/checktoken", {
+        headers: {"Content-Type": "application/json", "Authorization": cookies['Authorization-cookie']}
+      })
+      if (res.status === 200) redirect();
+    }
+    check();
+    setPageLoading(false);
+  }, [])
 
   const handleLogin = async () => {
     const res = await fetch("http://localhost:3001/account/login", {
@@ -22,21 +42,32 @@ export default function Login() {
     })
     const data = await res.json();
     console.log(data);
-    // console.log(loginPassword)
+    if (res.status !== 200) {
+      setErrorMessage(data.message)
+      alert("error: " + data.message);
+    } else {
+      setCookie('Authorization-cookie', data.token);
+      redirect();
+    }
+
   }
+
+  if (pageLoading) return <Loading />
+  
+ 
 
   return (
     <div>
-  <div className=" absolute top-[45%] left-[50%] translate-x-[-50%] translate-y-[-60%] flex flex-col align-middle justify-evenly m-auto w-72 h-80 text-gray-200 blur-none">
+  <div className=" absolute top-[45%] left-[50%] translate-x-[-50%] translate-y-[-60%] flex flex-col align-middle justify-evenly m-auto w-72 h-80 text-gray-200 blur-none ">
       
     <h1 className="font-normal text-3xl text-center mb-5">Login</h1>   
         <Input icon={<MailIcon />} labelText="Email" changeFunction={setLoginEmail}></Input>
     <Input icon={<LockClosedIcon/>} labelText="Password" type="password" changeFunction={setLoginPassword}></Input>
         <Button actionFunction={handleLogin}>Login</Button>
-    <p className="text-xs text-center mt-6">Don't have an account already? <Link to="/register" className="text-amber-300 text-sm italic ml-1 :hover">Register now</Link></p>
+    <p className="text-xs text-center mt-6">Don't have an account already? <Link to="/register" className="text-amber-300 text-sm font-semibold italic ml-1 :hover">Register now</Link></p>
+        {/* <p>{errorMessage}</p> */}
   </div>
       <div>
-        
         </div>
   </div>
   )
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,28 +1,81 @@
 import { LockClosedIcon, UserIcon, MailIcon } from '@heroicons/react/solid'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 //components
 import Input from '../components/Input';
 import Button from '../components/Button';
+import Loading from '../components/Loading';
+
+import { useCookies } from 'react-cookie';
 
 
+
+import { useState, useEffect } from 'react';
 
 export default function Register() {
+  const [ registerName, setRegisterName ] = useState("");
+  const [ registerEmail, setRegisterEmail ] = useState("");
+  const [ registerPassword, setRegisterPassword ] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [pageLoading, setPageLoading] = useState(true);
+
+  const [cookies, setCookie, removeCookie] = useCookies(['Authorization-cookie'])
+  
+  
+  const navigate = useNavigate();
+  const redirect = () => navigate("/dashboard");
+
+  useEffect(() => {
+    async function check() {
+      if (await cookies['Authorization-cookie'] === undefined) return;
+      const res = await fetch("http://localhost:3001/account/checktoken", {
+        headers: {"Content-Type": "application/json", "Authorization": cookies['Authorization-cookie']}
+      })
+      if (res.status === 200) redirect();
+    }
+
+    check();
+    setPageLoading(false);
+  }, [])
+
+
+
+  const handleRegister = async () => {
+    const res = await fetch("http://localhost:3001/account/register", {
+      method: "POST",
+      body: JSON.stringify({ name: registerName, email: registerEmail, password: registerPassword }),
+      headers: {"Content-Type": "application/json"}
+    })
+    const data = await res.json();
+    console.log(data);
+    if (res.status !== 200) {
+      setErrorMessage(data.body.message)
+      alert("error: " + data.body.message);
+    } else {
+      setCookie('Authorization-cookie', data.token);
+      redirect();
+    }
+  }
+
+  if (pageLoading) return <Loading />
+  
 
   return (
 
       
-    <div className=" absolute top-[48%] left-[50%] translate-x-[-50%] translate-y-[-60%] flex flex-col align-middle justify-evenly m-auto w-72 h-3/5 text-gray-200 blur-none">
+    <div className=" absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-60%] flex flex-col align-middle justify-evenly m-auto w-72 h-96 text-gray-200 blur-none ">
+
+{/* absolute top-[45%] left-[50%] translate-x-[-50%] translate-y-[-60%] flex flex-col align-middle justify-evenly m-auto w-72 h-80 text-gray-200 blur-none"> */}
       
         <h1 className="font-normal text-3xl text-center mb-5">Register</h1>
-        <Input icon={<UserIcon />} labelText="Name"></Input>
-        <Input icon={<MailIcon/>} labelText="Email"></Input>
-        <Input icon={<LockClosedIcon/>} labelText="Password" type="password"></Input>
-        <Button>Register</Button>
-        <p className="text-xs text-center mt-6">Have an account already? <Link to="/login" className="text-amber-300 text-sm italic ml-1 :hover">Login</Link></p>
+        <Input icon={<UserIcon />} labelText="Name" changeFunction={setRegisterName}></Input>
+        <Input icon={<MailIcon/>} labelText="Email" changeFunction={setRegisterEmail}></Input>
+        <Input icon={<LockClosedIcon/>} labelText="Password" type="password" changeFunction={setRegisterPassword}></Input>
+        <Button actionFunction={handleRegister}>Register</Button>
+        <p className="text-xs text-center mt-6">Have an account already? <Link to="/login" className="text-amber-300 text-sm font-semibold italic ml-1 :hover">Login</Link></p>
     </div>
          
-      
+ 
   )
 }
 
