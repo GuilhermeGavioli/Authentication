764f072403bc6bfb96c0186f578dd04249278b6f
Guilherme Gavioli <xiiguilhermeiix@gmail.com>
2022-05-22 23:35:52 -0300

    split userModel in a separated file

---
+++ backend/database/UserModel.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+const userSchema = new mongoose.Schema({
+    //add id later on...
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    }
+})
+export const UserModel = mongoose.model('UserModel', userSchema)
